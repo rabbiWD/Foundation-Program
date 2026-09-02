ea36e0a70341e8a7e63ace62c26f336b112557b1
Rabbi Khan <Krabby2244@gmail.com>
2026-09-02 15:40:36 +0600

    CNG fare meter done

--- answer.js
+++ answer.js
@@ -37,6 +37,21 @@ function validateUsername(username) {
   return "Available";
 }
 
+// Question No. 4: Dhaka CNG Fare Meter
+function getCngFare(distance, isNight = false, waitingMinutes = 0) {
+  let fare = 50;
+  if (distance > 2) {
+    fare = fare + (distance - 2) * 15;
+  }
+  fare = fare + waitingMinutes * 2;
+  if (isNight) {
+    fare = fare * 1.2;
+  }
+  return fare;
+}
+
+
+
 
 
 
