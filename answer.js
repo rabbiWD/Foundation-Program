35990a0840df1e173c54eff6a7241bb2353b3258
Rabbi Khan <Krabby2244@gmail.com>
2026-09-02 15:39:12 +0600

    Bangladesh weekend machine done

--- answer.js
+++ answer.js
@@ -5,4 +5,24 @@ function describeValue(value) {
   return `${type} | ${truthiness}`;
 }
 
+// Question No. 2: Bangladesh Weekend Machine
+function getDayType(day) {
+  const formattedDay = String(day).toLowerCase();
+  switch (formattedDay) {
+    case "friday":
+    case "saturday":
+      return "Weekend";
+    case "sunday":
+    case "monday":
+    case "tuesday":
+    case "wednesday":
+    case "thursday":
+      return "Working Day";
+    default:
+      return "Invalid Day";
+  }
+}
+
+
+
 
