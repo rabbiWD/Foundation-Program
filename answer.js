d8d59a3d4fd7a85f662b9498cee87a2c68e22dd2
Rabbi Khan <Krabby2244@gmail.com>
2026-09-02 15:39:57 +0600

    username gatekeeper done

--- answer.js
+++ answer.js
@@ -23,6 +23,22 @@ function getDayType(day) {
   }
 }
 
+// Question No. 3: Username Gatekeeper
+function validateUsername(username) {
+  if (username.length < 4) {
+    return "Too Short";
+  }
+  if (username.includes(" ")) {
+    return "No Space Allowed";
+  }
+  if (username.toLowerCase().includes("admin")) {
+    return "Reserved Word";
+  }
+  return "Available";
+}
+
+
+
 
 
 
